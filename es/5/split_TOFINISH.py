52ae2b80855d6b1149b6696214ed5937bd0f790d
nilou <githubppv@gmail.com>
2025-10-13 08:25:56 +0200

    changes

--- es/5/split_TOFINISH.py
+++ es/5/split_TOFINISH.py
@@ -1,11 +1,15 @@
 def split_string(string: str, characters: str = '') -> list[str]:
-	list(string)
-	new_string = []
-	for index in range(len((string))):
-		if characters in string:
-			new_string[i] = string[:index] 
-			
+    list(string)
+    new_string = []
+    if characters in string:
+        for i in range(len(string)):
+            if string[i] in characters:
+                new_string = string[:i] #TODO: enable multiple words + 
+                                        #add new_string[i] in some way
+    else:
+        print(f"no {characters} in {string}")
+    print(new_string)
 
 #print(string.split("o")) desired output:['cia', ' c', 'me va?']
 
-split_string("ciao come va?", "o")
+split_string("ciao", "o")
