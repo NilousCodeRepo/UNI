d325751631797c46ff7437756bc17a1129ee4c5a
Nilou <githubppv@gmail.com>
2026-05-31 19:52:18 +0200

    Updated run script

--- Y1S2/Metodologie_di_Programmazione/project/run.sh
+++ Y1S2/Metodologie_di_Programmazione/project/run.sh
@@ -2,15 +2,23 @@
 
 #set -xe
 
+if ! [ -z $1 ];then
+    COMP_OPT="-Werror -g:none -Xlint"
+
+    if [ "$1" = 'v' ];then
+        COMP_OPT="-Werror -g:none -verbose -Xlint"
+    fi
+fi
+
 SOURCE_PATH="src/*.java"
 CLASS_PATH="cls"
 
 MOD_PATH="--module-path ${JAVA_FX_PATH}" 
 MOD_OPT="--add-modules javafx.controls"
 
-javac ${MOD_PATH} ${MOD_OPT} -d ${CLASS_PATH} ${SOURCE_PATH}
+javac ${COMP_OPT} ${MOD_PATH} ${MOD_OPT} -d ${CLASS_PATH} ${SOURCE_PATH}
 
-if ! [ -z $1 ];then
+if ! [[ -z $1 ]] && { [[ "$1" != 'v' ]] || [[ "$1" = ' ' ]]; };then
     if [ "$1" = 'r' ]; then
         if ! [ -z $2 ]; then
             java ${MOD_PATH} ${MOD_OPT} --enable-native-access=javafx.graphics -cp "${CLASS_PATH}" $2
