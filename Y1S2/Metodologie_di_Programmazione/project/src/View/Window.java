4c217ce4205b61cbe9a3b00f8317398719cf2780
Nilou <githubppv@gmail.com>
2026-06-04 15:17:01 +0200

    thingds

--- Y1S2/Metodologie_di_Programmazione/project/src/View/Window.java
+++ Y1S2/Metodologie_di_Programmazione/project/src/View/Window.java
@@ -4,9 +4,6 @@ import javafx.stage.Stage;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;//usalo per la root, mette gli elementi uno sopra l'altro
-import javafx.scene.layout.VBox; // usalo per roba tipo liste di bottoni uno sotto/accanto l'altro 
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
 
 public class Window
 {
